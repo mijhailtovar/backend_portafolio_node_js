6335caa56089874d4a348d857573014ef4a6b74e
mijhail <mijhailtovar@gmail.com>
2025-10-23 13:28:42 -0400

    cambio para conectarse a la base de datos remota de mongo atlas

--- index.js
+++ index.js
@@ -2,16 +2,24 @@
 
 var mongoose = require('mongoose');
 var app = require('./app');
-var port = 3900;
+
+// ** 1. Usar la variable de entorno PORT, o 3900 por defecto **
+var port = process.env.PORT || 3900; 
+
+// ** 2. Usar la variable de entorno MONGODB_URI, o la local por defecto **
+var url = process.env.MONGODB_URI || 'mongodb://localhost:27017/api_rest_blog';
 
 mongoose.set('useFindAndModify', false);
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/api_rest_blog', {useNewUrlParser: true}) 
+
+// Conectar usando la variable 'url'
+mongoose.connect(url, {useNewUrlParser: true}) 
     .then(() => {
-        console.log('coneccion a la base de datos se realizo correctamente');
+        console.log('Conexión a la base de datos se realizó correctamente');
 
         // Crear servidor y ponerme a escuchar peticiones http
         app.listen(port, () => {
-            console.log('Servidor corrientdo en http://localhost:'+port);
+            console.log('Servidor corriendo en el puerto: ' + port);
         });
-    });
+    })
+    .catch(error => console.log(error)); // Añadir manejo de errores por si falla la conexión
